da504c8367d599cde5939e6b0a7d9c1daeea7699
Julien <julien.genty.contact@gmail.com>
2026-03-04 14:48:28 +0100

    config (fonts): initialize of fonts in the project

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function App() {
-  return (
+  const [fontLoaded] = useFonts({
+    Light: require("./assets/fonts/Montserrat-Light.ttf"),
+    Regular: require("./assets/fonts/Montserrat-Regular.ttf"),
+    Medium: require("./assets/fonts/Montserrat-Medium.ttf"),
+    SemiBold: require("./assets/fonts/Montserrat-SemiBold.ttf"),
+  });
+
+  return fontLoaded ? (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: "Light" }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text style={{ fontFamily: "Regular" }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text style={{ fontFamily: "Medium" }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text style={{ fontFamily: "SemiBold" }}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
-  );
+  ) : null;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
